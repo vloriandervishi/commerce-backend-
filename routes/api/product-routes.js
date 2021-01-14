ef40bd46e34e66687de128cdee6c8ee1a47a489e
Valdo <vlorian22@GMAIL.COM>
2021-01-13 20:16:53 -0600

    added finda all products code into product-route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,6 +7,19 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+  include:[{
+    model:Category,
+    attributes:['category_name']
+  },
+  {
+    model:Tag,
+    attributes:['tag_name']
+  }
+  
+    
+]
+  });
 });
 
 // get one product
